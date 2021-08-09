f4a7341ba83df3586e3082d375c6641e232d8901
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-09 16:52:57 +0000

    add client

---
+++ TCPClient.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import socket
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# host = '192.168.1.22'
+host = socket.gethostname()
+port = 5000
+
+clientsocket.connect((host, port))
+
+# max amount of data client accepts at once
+message = clientsocket.recv(1024)
+
+clientsocket.close()
+
+print(message.decode("ascii"))
---
+++ TCPServer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# host='192.168.1.22'
+host = socket.gethostname()
+port = 5000
+
+serversocket.bind(('192.168.1.22', port))
+
+serversocket.listen(3)
+
+while True:
+    clientsocket, address = serversocket.accept()
+
+    print("Connection received from %s " % str(address))
+
+    message = 'Thank you for connecting to the server. Test data.' + '\r\n'
+    clientsocket.send(message.encode('ascii'))
+
+    clientsocket.close()
